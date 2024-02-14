1f3ce5e2570c3eb54a300aa69fbab73dd1b30eeb
ankitapawar0608 <kedarpawar775@gmail.com>
2024-02-14 16:34:31 +0530

    Todo app design

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,23 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
 import imgadd from "./button.png"
 
 function Home() {
+  
+
+
   return (
     <div>
-         <div class="todo-container">
+      <div class="todo-container ">
         <div class="to-do-title">
-            ToDo App
+          ToDo App
         </div>
+
+
 
         <div>
-            <input type='text' placeholder='Add a new task' className='p-2 m-3 w-75'/>
-           
-            <img src={imgadd} height={40} className='add-btn' alt='add'/>
+          <input 
+          type='text' 
+          placeholder='Add a new task' 
+          className='p-2 m-3 w-75 input' 
+          value={newtask}
+          onChange={(e)=>{
+            setNewtask(e.target.value)
+          }}
+          />
+
+          <img 
+          src={imgadd} 
+          height={40} 
+          className='add-btn' 
+          alt='add' 
+          onClick={addTask}
+          />
         </div>
 
-        
-    </div>
+        <div className='d-flex flex-column align-items-center overflow-y-scroll h-75 '>
+          {
+            tasks.map((task, i) => {
+              return (
+               <p key={i} className='border border-2 border-info text-center w-75 rounded-pill fw-normal  fs-4'>
+                 <h4>
+                  {task}
+                </h4>
+               </p>
+                )
+            })
+          }
+        </div>
+
+      </div>
 
 
     </div>
